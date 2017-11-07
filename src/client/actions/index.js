e67b0b16cef899ea326b694ec5197a7bba426619
Renan Pallin <renanpallin@gmail.com>
2017-11-07 17:51:57 -0200

    Rendering a list of users on client

---
+++ src/client/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const API = 'https://react-ssr-api.herokuapp.com/';
+
+export const FETCH_USERS = 'FETCH_USERS';
+export const fetchUsers = () => async dispatch => {
+	const res = await axios.get(API + 'users');
+	dispatch({
+		type: FETCH_USERS,
+		payload: res,
+	});
+};
---
+++ src/client/components/UsersList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UsersList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	componentDidMount() {
+		this.props.fetchUsers();
+	}
+
+	renderUsers() {
+		return this.props.users.map(user => {
+			return <li key={user.id}>{user.name}</li>;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				Here's a big list of users:
+				<ul>{this.renderUsers()}</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return { users: state.users };
+}
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
