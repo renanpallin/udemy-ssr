f30a1367e0fc7115892fb22860748fa6c48e1b24
Renan Pallin <renanpallin@gmail.com>
2017-11-07 15:12:07 -0200

    Webpack for client and server

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-	return <div>I'm the home component!</div>
-}
+	return (
+		<div>
+			<div>I'm the home component!</div>
+			<button onClick={e => console.log('apertou!')}>Press me!</button>
+		</div>
+	);
+};
 
 export default Home;
