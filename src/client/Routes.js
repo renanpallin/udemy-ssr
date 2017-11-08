6b6b692ffd47f1e2d3968c6fd00828b5bf404e48
Renan Pallin <renanpallin@gmail.com>
2017-11-08 14:34:55 -0200

    Adding react-router-config to isomorfic routes on project

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Home from './components/Home';
 import UsersList from './components/UsersList';
 
-export default () => {
-	return (
-		<div>
-			<Route exact path="/" component={Home} />
-			<Route exact path="/users" component={UsersList} />
-		</div>
-	);
-}
+const exact = true;
+export default [
+	{
+		path: '/',
+		component: Home,
+		exact,
+	},
+	{
+		path: '/users',
+		component: UsersList,
+	},
+];
