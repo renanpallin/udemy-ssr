5159f9b956215aad307f8966fecc21b068580bf2
Renan Pallin <renanpallin@gmail.com>
2017-11-13 17:00:15 -0200

    Header with auth logic

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-const Header = ({ auth }) => (
-	<div>
-		<Link to="/">React SSR</Link>
-		<h2>{JSON.stringify(auth)}</h2>
-	</div>
-);
+const Header = ({ auth }) => {
+	const authButton = auth ? (
+		<a href="/api/logout">Logout</a>
+	) : (
+		<a href="/api/auth/google">Login</a>
+	);
+	return (
+		<div>
+			<Link to="/">React SSR</Link>
+			<h2>{JSON.stringify(auth)}</h2>
+			<div>
+				<Link to="/users">Users</Link>
+				<Link to="/admins">Admins</Link>
+				{authButton}
+			</div>
+		</div>
+	);
+};
 
 export default connect(({ auth }) => ({ auth }))(Header);
