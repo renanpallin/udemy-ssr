c41cb723e50e234f13c5a4deedfa0f0d80980907
Renan Pallin <renanpallin@gmail.com>
2017-11-16 13:07:07 -0200

    Not found page

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,21 +1,19 @@
 import App from './App';
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
+import NotFountPage from './pages/NotFoundPage';
 
 const exact = true;
-export default [
-	{
-		...App,
-		routes: [
-			{
-				...HomePage,
-				path: '/',
-				exact,
-			},
-			{
-				...UsersListPage,
-				path: '/users',
-			},
-		],
-	},
-];
+export default [{
+	...App,
+	routes: [{
+		...HomePage,
+		path: '/',
+		exact,
+	}, {
+		...UsersListPage,
+		path: '/users',
+	}, {
+		...NotFountPage,
+	}],
+}];
---
+++ src/client/pages/NotFoundPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => <h1>Ooooops, route not found.</h1>;
+
+export default {
+	component: NotFoundPage,
+};
