3ab6c14f6171fd1d79b6a021e0f4c12b0da010dd
Renan Pallin <renanpallin@gmail.com>
2017-11-16 13:31:53 -0200

    Status 404 for not found page

--- src/client/pages/NotFoundPage.js
+++ src/client/pages/NotFoundPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const NotFoundPage = () => <h1>Ooooops, route not found.</h1>;
+const NotFoundPage = ({ staticContext = {} }) => {
+	staticContext.notFound = true;
+	return <h1>Ooooops, route not found.</h1>;
+};
 
 export default {
 	component: NotFoundPage,
--- src/index.js
+++ src/index.js
@@ -30,7 +30,13 @@ app.get('*', (req, res) => {
 
 	Promise.all(promises)
 		.then(() => {
-			res.send(renderer(req, store));
+			const context = {};
+			const content = renderer(req, store, context);
+
+			if (context.notFound)
+				res.status(404);
+
+			res.send(content);
 		})
 		.catch((e) => res.send('shit happens...\n' + e));
 });
